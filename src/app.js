d8fca83d1132c9f61576d274f5e056046dcd6c38
Mauri Gon <gonmauri89@gmail.com>
2024-09-02 02:06:31 +0000

    subida completa de archivos

--- src/app.js
+++ src/app.js
@@ -7,5 +7,18 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
-};
+            window.onload = () => {
+              document.getElementById("excuse").innerHTML = generateExcuse();
+              
+          }
+
+const generateExcuse = () => {
+              let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+              let action = ['ate', 'peed', 'crushed', 'broke'];
+              let what = ['my homework', 'my phone', 'the car'];
+              let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
+              let excuse = `${who[Math.floor(Math.random() * who.length)]} ${action[Math.floor(Math.random() * action.length)]} ${what[Math.floor(Math.random() * what.length)]} ${when[Math.floor(Math.random() * when.length)]}`;
+
+
+              document.getElementById("excuse").innerHTML = excuse;
+  };
